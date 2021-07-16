67694a034752e1226521d99abb749993f93012e6
Julia <2939599980@qq.com>
2021-07-16 15:13:03 +0800

    modify...

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<!--
+*Author: Eloise
+*Date: 2021-07-16 15:11
+-->
+<template>
+  <div>
+    home.vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<!--
+*Author: Eloise
+*Date: 2021-07-16 15:11
+-->
+<template>
+  <div>
+    login.vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
